5ad73aeae3c8208ca10c7b0d146fdf310f61e0aa
Ashling <114626446+AshlingnLog@users.noreply.github.com>
2023-02-08 13:09:20 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Formik, Field, Form } from "formik";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sign Up</h1>
+      <Formik
+        initialValues={{ name: "", email: "" }}
+        onSubmit={async (values) => {
+          await new Promise((resolve) => setTimeout(resolve, 500));
+          alert(JSON.stringify(values, null, 2));
+          alert("Login Successful");
+        }}
+
+        // Figure out how to add exception
+        // validate: (values) => {
+        //  let errors = {};
+        //  if (!values.email) errors.email = "field required";
+        //if (!values.password) errors.password = "field required";
+        // return errors;
+      >
+        <Form>
+          <div> Name </div>
+          <Field name="name" type="text" />
+          <div> Email </div>
+          <Field name="email" type="email" />
+          <div> Password </div>
+          <Field name="password" type="password" />
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
